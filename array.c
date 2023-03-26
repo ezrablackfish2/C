ce9f4e88b414e69eba1c48cd5d73000044742550
ezra <ezrayeneneh1992@gmail.com>
2023-03-26 00:04:26 -0700

    array of all time

--- array.c
+++ array.c
@@ -8,11 +8,17 @@ int main(void)
 {
 	int ezra[10];
 	int *point;
+	int i;
 
 	point = &ezra[0];
 	*point = 10;
 	printf("%d\n", ezra[0]);
-	*(point + 1) = 20;
+	*(point + 1) = 10;
 	printf("%d\n", ezra[1]);
+	for (i = 1; i < 10; i++)
+	{
+		*(point + i) = *(point + (i-1)) + 10;
+		printf("%d \t \a \n", ezra[i]);
+	}
 	return (0);
 }
